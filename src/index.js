9120141010920443cbbbd2ebd700996f913b7248
John <shoemjoh@gmail.com>
2024-01-28 16:46:45 -0500

    working fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => console.log("we are connected!!"))
+// COMMIT ABOUT 30 TIMES DURING THIS PROJECT, PRESENT TENSE
+
+document.addEventListener("DOMContentLoaded", () => getDestinations())
+// Do we want to immediately invoke fetchData there.
+
+// Using mock backend using db.json server instead of remote API.
+function getDestinations() {
+    console.log("get destinations")
+    fetch('http://localhost:3000/destinations')
+        .then((resp) => resp.json())
+        // Parse with json method
+        .then((data) => console.log(data))
+    // Open index.html to see datastructure in console. Make second commit "working fetch" git add ., git commit -m "", git push
+}
