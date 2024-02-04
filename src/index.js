8d13d0fb7e8ea06f81a1cc4bb16986a85d77c5e6
John <shoemjoh@gmail.com>
2024-02-04 12:27:37 -0500

    working html tile creation from destinations stored in json database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 // COMMIT ABOUT 30 TIMES DURING THIS PROJECT, PRESENT TENSE
 
-document.addEventListener("DOMContentLoaded", () => getDestinations())
-// Do we want to immediately invoke fetchData there.
+document.addEventListener("DOMContentLoaded", () => {
 
-// Using mock backend using db.json server instead of remote API.
-function getDestinations() {
-    console.log("get destinations")
-    fetch('http://localhost:3000/destinations')
-        .then((resp) => resp.json())
-        // Parse with json method
-        .then((data) => console.log(data))
-    // Open index.html to see datastructure in console. Make second commit "working fetch" git add ., git commit -m "", git push
-}
+    // Using mock backend using db.json server instead of remote API.
+    function getDestinations() {
+        console.log("get destinations")
+        // Grab html element where we want to display the server request.
+
+        // HTTP GET request to local server
+        // Run json-server --watch db.json in the terminal
+        fetch('http://localhost:3000/destinations')
+            .then((resp) => resp.json())
+            // Parse with json method
+            .then((data) => {
+                initializeDestinations(data)
+            })
+    }
+    // Render each of our destinations to the DOM
+    function initializeDestinations(data) {
+        console.log()
+        data.forEach(destination => renderOneDestination(destination))
+    }
+
+    // DOM Render Functions
+    function renderOneDestination(destination) {
+        // Build city tile, line up to CSS style names for tile
+        let tile = document.createElement('li')
+        tile.className = 'tile'
+        tile.innerHTML = `
+    <div>
+        <h4>${destination.name}<h4>
+    </div>
+    `
+        // Add city tile to DOM
+        document.querySelector('#destination-list').appendChild(tile)
+
+        console.log(tile);
+    }
+    getDestinations();
+});
+
